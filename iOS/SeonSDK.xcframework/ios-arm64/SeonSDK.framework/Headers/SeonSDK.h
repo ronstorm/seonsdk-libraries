fffa0fc3c2d4da1ebf2ac2383589c0e64c8e6a0f
Amit Sen <amit.ron.cse@gmail.com>
2024-09-22 22:27:23 +0200

    The SeonSDK  libraries for iOS and Android

---
+++ iOS/SeonSDK.xcframework/ios-arm64/SeonSDK.framework/Headers/SeonSDK.h
@@ -0,0 +1,18 @@
+//
+//  SeonSDK.h
+//  SeonSDK
+//
+//  Created by Amit on 14.09.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SeonSDK.
+FOUNDATION_EXPORT double SeonSDKVersionNumber;
+
+//! Project version string for SeonSDK.
+FOUNDATION_EXPORT const unsigned char SeonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeonSDK/PublicHeader.h>
+
+
